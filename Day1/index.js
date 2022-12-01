dd4a85a8adec1a23857bb9d6f544939fc8eda7ea
Voidarchive <voidarchived@gmail.com>
2022-12-01 12:35:59 +0545

    Day 1 Complete

---
+++ Day1/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const data = (0, fs_1.readFileSync)('./input.txt');
+let a = data.toString().split('\n\n'); // Split the string with two new line
+let array = [];
+for (let i = 0; i < a.length; i++) {
+    array.push(a[i]
+        .split('\n') // again split the string to array by new line
+        .map((i) => Number(i)) // Parse the string to number
+        .reduce((a, b) => a + b) // Find of sum of a single elf
+    );
+}
+//Part One answer 71300
+// console.log(Math.max(...array));
+function sumThreeLargest(array) {
+    array.sort((a, b) => a - b);
+    array.reverse();
+    return array[0] + array[1] + array[2];
+}
+console.log(sumThreeLargest(array));
---
+++ Day1/index.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from 'fs';
+
+const data = readFileSync('./input.txt');
+
+let a = data.toString().split('\n\n'); // Split the string with two new line
+
+let array = [];
+for (let i = 0; i < a.length; i++) {
+  array.push(
+    a[i]
+      .split('\n') // again split the string to array by new line
+      .map((i) => Number(i)) // Parse the string to number
+      .reduce((a, b) => a + b) // Find of sum of a single elf
+  );
+}
+
+//Part One answer 71300
+// console.log(Math.max(...array));
+
+function sumThreeLargest(array: number[]) {
+  array.sort((a, b) => a - b);
+  array.reverse();
+
+  return array[0] + array[1] + array[2];
+}
+
+console.log(sumThreeLargest(array));
