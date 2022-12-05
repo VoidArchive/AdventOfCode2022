1fd1b1e5f028d393fdc89f6083080326b48b6a63
Voidarchive <voidarchived@gmail.com>
2022-12-05 11:17:04 +0545

    day3

---
+++ Day3/index.js
@@ -0,0 +1,39 @@
+import { readFileSync } from 'fs';
+const data = readFileSync('input.txt').toString().split('\n');
+// Part One
+const getValueOfChar = (char) => {
+    return char === char.toUpperCase()
+        ? char.charCodeAt(0) - 38
+        : char.charCodeAt(0) - 96;
+};
+const partOne = data.reduce((sum, word) => {
+    const [a, b] = [
+        word.substring(0, word.length / 2),
+        word.substring(word.length / 2),
+    ];
+    for (let i = 0; i < a.length; i++) {
+        if (b.includes(a[i])) {
+            return (sum += getValueOfChar(a[i]));
+        }
+    }
+}, 0);
+console.log('Part 1:', partOne);
+const splitArrayAfterNValues = (array, length) => {
+    let result = [];
+    let amountOfLoops = array.length / length;
+    for (let i = 0; i < amountOfLoops; i++) {
+        const downRange = i * length;
+        const upperRange = (i + 1) * length;
+        result = [...result, array.slice(downRange, upperRange)];
+    }
+    return result;
+};
+const partTwo = splitArrayAfterNValues(data, 3).reduce((sum, wordTriplet) => {
+    const [a, b, c] = wordTriplet;
+    for (let j = 0; j < a.length; j++) {
+        if (b.includes(a[j]) && c.includes(a[j])) {
+            return (sum += getValueOfChar(a[j]));
+        }
+    }
+}, 0);
+console.log(splitArrayAfterNValues(data, 3));
