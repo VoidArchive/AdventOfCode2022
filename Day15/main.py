ae4dd1152c756e288492e2291f5a2e18f3055191
Voidarchive <voidarchived@gmail.com>
2022-12-15 12:00:50 +0545

    day15

---
+++ Day15/main.py
@@ -0,0 +1,32 @@
+
+import sys
+import fileinput
+import re
+import math
+
+i = [list(map(int, re.findall("-?\d+", l)))
+     for l in fileinput.input('input.txt')]
+s = {(sx, sy, abs(sx - bx) + abs(sy - by)) for sx, sy, bx, by in i}
+# Part 1
+xl = math.inf
+xh = -math.inf
+for sx, sy, sc in s:
+    dx = sc - abs(2000000 - sy)
+    if dx > 0:
+        xl = min(xl, sx - dx)
+        xh = max(xh, sx + dx)
+print(xh - xl)
+
+# Part 2
+for sx, sy, sc in s:
+    for p in range(sc + 1):
+        for tx, ty in ((sx - sc - 1 + p, sy - p),
+                       (sx + sc + 1 - p, sy - p),
+                       (sx - sc - 1 + p, sy + p),
+                       (sx + sc + 1 - p, sy + p)):
+            if (0 <= tx <= 4000000 and
+                0 <= ty <= 4000000 and
+                all(abs(tx - ox) + abs(ty - oy) > od
+                    for ox, oy, od in s)):
+                print(tx * 4000000 + ty)
+    break
